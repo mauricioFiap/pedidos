4178e07de3f3118ee8682adace9908481e6d7621
mauricioaramos <mauramosaraujo@gmail.com>
2024-05-20 23:26:45 -0300

    api

--- msgestaopedidos/src/test/java/br/com/fiap/msgestaopedidos/MsgestaopedidosApplicationTests.java
+++ msgestaopedidos/src/test/java/br/com/fiap/msgestaopedidos/MsgestaopedidosApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package br.com.fiap.msgestaopedidos;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class MsgestaopedidosApplicationTests {
 	}
 
 }
+*/
--- mslogisticaentrega/src/test/java/br/com/fiap/mslogisticaentrega/MslogisticaentregaApplicationTests.java
+++ mslogisticaentrega/src/test/java/br/com/fiap/mslogisticaentrega/MslogisticaentregaApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package br.com.fiap.mslogisticaentrega;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class MslogisticaentregaApplicationTests {
 	}
 
 }
+*/
